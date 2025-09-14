5c30ad201076f80254c9f22ad847fc1dac51bb8c
Induaditya3 <o0bvc52p@duck.com>
2025-09-14 09:58:56 +0530

    fix test by passing str in stdin instead of  int

--- pascal/t.py
+++ pascal/t.py
@@ -8,14 +8,14 @@ def exists():
 @check50.check(exists)
 def prints_level_1():
     """prints first level of pascal triangle correctly"""
-    check50.run("python3 pascal.py").stdin(1).stdout("[1]\n").exit(0)
+    check50.run("python3 pascal.py").stdin("1").stdout("[1]\n").exit(0)
 
 @check50.check(exists)
 def prints_level_2():
     """prints level 2 of pascal triangle correctly"""
-    check50.run("python3 pascal.py").stdin(2).stdout("[1, 2, 1]\n").exit(0)
+    check50.run("python3 pascal.py").stdin("2").stdout("[1, 2, 1]\n").exit(0)
 
 @check50.check(exists)
 def print_level_5():
     """prints level 5 of pascal triangle correctly"""
-    check50.run("python3 pascal.py").stdin(5).stdout("[1, 5, 10, 10, 5, 1]\n").exit(0)
+    check50.run("python3 pascal.py").stdin("5").stdout("[1, 5, 10, 10, 5, 1]\n").exit(0)
